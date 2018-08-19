2de5faa1168a18b7f9fdffefa5ce9a40b14d8150
Gero <gero.embser@gmail.com>
2018-08-19 19:07:45 +0200

    Initial framework setup

---
+++ AdvancedNavigationController.podspec
@@ -0,0 +1,17 @@
+Pod::Spec.new do |s|
+s.name = 'AdvancedNavigationController'
+s.version = '1.0'
+s.author = 'Gero Embser'
+s.homepage = 'https://github.com/geroembser/AdvancedNavigationController'
+s.license = { :type => 'MIT', :file => 'LICENSE' }
+
+s.summary = 'An advanced UINavigationController with very few advanced features...'
+
+s.source = { :git => 'https://github.com/geroembser/AdvancedNavigationController.git', :tag => s.version }
+
+s.ios.deployment_target = '12.0'
+
+#define the source files
+s.source_files = 'Source/*.swift'
+
+end
---
+++ Source/AdvancedNavigationController.h
@@ -0,0 +1,19 @@
+//
+//  AdvancedNavigationController.h
+//  AdvancedNavigationController iOS
+//
+//  Created by Gero Embser on 19.08.18.
+//  Copyright © 2018 Gero Embser. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for AdvancedNavigationController.
+FOUNDATION_EXPORT double AdvancedNavigationControllerVersionNumber;
+
+//! Project version string for AdvancedNavigationController.
+FOUNDATION_EXPORT const unsigned char AdvancedNavigationControllerVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Source/PublicHeader.h>
+
+
